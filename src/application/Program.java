d0b3d985bf3f809ec58ea606237e93d2d8ebf2dc
Yuyake23 <brunosamuel.orizona@gmail.com>
2022-12-23 15:45:40 -0300

    add Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department d1 = new Department(1, "Books");
+		Seller s1 = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000d, d1);
+
 		System.out.println(d1);
+		System.out.println(s1);
 	}
 
 }
